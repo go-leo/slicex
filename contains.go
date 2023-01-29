eb95d14317870075aa863cb849ac51db41dba6a8
soyacen <soyacen@outlook.com>
2023-01-29 16:31:04 +0800

    fix ContainsFunc

--- contains.go
+++ contains.go
@@ -3,6 +3,6 @@ package slicex
 import "golang.org/x/exp/slices"
 
 // ContainsFunc reports whether v is present in s.
-func ContainsFunc[E comparable](s []E, f func(E) bool) bool {
-	return slices.IndexFunc(s, f) >= 0
+func ContainsFunc[E any](s []E, f func(E) bool) bool {
+	return slices.ContainsFunc(s, f)
 }
